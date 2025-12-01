eed970c4b9cef60361841b0a8691db2f0a895a3f
Jacinth <jacinthcedricbarral@gmail.com>
2025-12-02 03:19:21 +0800

    feat[Barral]: Added new routees

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,15 @@ import { default as DashboardLayout } from "./components/layouts/DashboardLayout
 import HomeLayout from "./components/layouts/HomeLayout";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import HavenlyDashboard from "./pages/Dashboard";
+import ForgotPassword from "./pages/ForgotPassword";
 import LandlordDashboard from "./pages/landlords/LandlordDashboard";
 import Login from "./pages/Login";
+import FeaturesPage from "./pages/public/FeaturesPublic";
+import LandingPage from "./pages/public/LandingPage";
+import LandlordsPage from "./pages/public/LandlordPublic";
+import TenantsPage from "./pages/public/TenantPublic";
+import Register from "./pages/Register";
+import ResetPassword from "./pages/ResetPassword";
 import TenantDashboard from "./pages/tenants/TenantDashboard";
 
 function App() {
@@ -14,19 +21,19 @@ function App() {
     <Router>
       <Routes>
         {/* Public Routes */}
-        <Route element={<HomeLayout />} path="/">
-          <Route element={<div>Landing Page</div>} />
+        <Route element={<HomeLayout />}>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/features" element={<FeaturesPage />} />
+          <Route path="/landlords" element={<LandlordsPage />} />
+          <Route path="/tenants" element={<TenantsPage />} />
         </Route>
 
         {/* Authentication Routes */}
         <Route element={<AuthLayout />} path="/">
           <Route path="login" element={<Login />} />
-          <Route path="register" element={<div>Register</div>} />
-          <Route path="forgot-password" element={<div>Forgot Password</div>} />
-          <Route
-            path="reset-password/:token"
-            element={<div>Reset Password</div>}
-          />
+          <Route path="register" element={<Register />} />
+          <Route path="forgot-password" element={<ForgotPassword />} />
+          <Route path="reset-password" element={<ResetPassword />} />
         </Route>
 
         {/* Role Protected Routes */}
