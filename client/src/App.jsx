e32fa41ecb6a1319b522a3ebc415d5105fc6f178
Jacinth <jacinthcedricbarral@gmail.com>
2025-12-05 00:59:06 +0800

    feat[Barral]: Added routes for landlords, tenants and admin, and added made sure each roles has their own protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,30 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Toaster } from "sonner";
 import "./App.css";
 import AuthLayout from "./components/layouts/AuthLayout";
 import { default as DashboardLayout } from "./components/layouts/DashboardLayout";
 import HomeLayout from "./components/layouts/HomeLayout";
+import AdminProtectedRoute from "./context/AdminProtectedRoute";
+import ProtectedRoute from "./context/ProtectedRoute";
 import AdminDashboard from "./pages/admin/AdminDashboard";
-import HavenlyDashboard from "./pages/Dashboard";
+import AdminUnderconstruction from "./pages/AdminUnderconstruction";
 import ForgotPassword from "./pages/ForgotPassword";
 import LandlordDashboard from "./pages/landlords/LandlordDashboard";
 import Login from "./pages/Login";
 import FeaturesPage from "./pages/public/FeaturesPublic";
 import LandingPage from "./pages/public/LandingPage";
 import LandlordsPage from "./pages/public/LandlordPublic";
-import TenantsPage from "./pages/public/TenantPublic";
+import { default as TenantsPage } from "./pages/public/TenantPublic";
 import Register from "./pages/Register";
 import ResetPassword from "./pages/ResetPassword";
 import TenantDashboard from "./pages/tenants/TenantDashboard";
+import Unauthorized from "./pages/Unauthorized";
+import Underconstruction from "./pages/UnderConstruction";
 
 function App() {
   return (
     <Router>
+      <Toaster richColors position="top-right" expand limit={2} />
       <Routes>
         {/* Public Routes */}
         <Route element={<HomeLayout />}>
@@ -37,12 +43,78 @@ function App() {
         </Route>
 
         {/* Role Protected Routes */}
-        <Route element={<DashboardLayout />}>
-          <Route path="/dashboard" element={<HavenlyDashboard />} />
+        {/* <Route element={<DashboardLayout />}>
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
           <Route path="/landlord/dashboard" element={<LandlordDashboard />} />
           <Route path="/tenant/dashboard" element={<TenantDashboard />} />
+        </Route> */}
+
+        {/* Admin Routes */}
+        <Route path="/admin/" element={<DashboardLayout />}>
+          <Route
+            path="dashboard"
+            element={
+              <AdminProtectedRoute>
+                <AdminDashboard />
+              </AdminProtectedRoute>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <AdminProtectedRoute>
+                <AdminUnderconstruction />
+              </AdminProtectedRoute>
+            }
+          />
         </Route>
+
+        {/* Tenant Routes */}
+        <Route path="/tenant/" element={<DashboardLayout />}>
+          <Route
+            path="dashboard"
+            element={
+              <ProtectedRoute allowedRoles={["tenant"]}>
+                <TenantDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <ProtectedRoute allowedRoles={["tenant"]}>
+                <Underconstruction />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+
+        {/* Landlord Routes */}
+        <Route path="/landlord/" element={<DashboardLayout />}>
+          <Route
+            path="dashboard"
+            element={
+              <ProtectedRoute allowedRoles={["landlord"]}>
+                <LandlordDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <ProtectedRoute allowedRoles={["landlord"]}>
+                <Underconstruction />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+
+        <Route path="/unauthorized" element={<Unauthorized />} />
+        {/* Root redirect based on role */}
+        {/* <Route path="/" element={<Navigate to="/login" replace />} /> */}
+
+        {/* Catch all - redirect to login */}
+        {/* <Route path="*" element={<Navigate to="/login" replace />} /> */}
       </Routes>
     </Router>
   );
