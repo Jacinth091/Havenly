d91f2362a680dc268e03529b1ed05092777b247d
Jacinth <jacinthcedricbarral@gmail.com>
2025-12-06 22:00:20 +0800

    feat[Barral]: Added GET routes, GET rooms per property, GET rooms from all properties, and GET rooms details by room id

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PropertyController;
+use App\Http\Controllers\RoomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
@@ -41,6 +42,15 @@ Route::prefix('/v1')->group(function () {
 
         Route::get('/properties', [PropertyController::class, 'getOwnedProperties'])
             ->middleware(['jwt.auth', 'jwt.role:landlord']);
+
+        Route::get('/properties/rooms', [RoomController::class , 'getAllRooms'])
+            ->middleware(['jwt.auth','jwt.role:landlord,admin']);
+
+        Route::get('/properties/{property_id}/rooms', [RoomController::class, 'getRoomByProperty'])
+            ->middleware(['jwt.auth', 'jwt.role:landlord,admin']);
+
+        Route::get('/properties/{property_id}/rooms/{room_id}', [RoomController::class, 'getRoomDetailsById'])
+            ->middleware(['jwt.auth', 'jwt.role:landlord,admin']);
 
         // Route::get('/property/{id}', [PropertyController::class, 'getProperties'])
         //     ->middleware('jwt.role:landlord');
