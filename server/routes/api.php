8df27f120be6969028aa619f807dbf820721d848
Jacinth <jacinthcedricbarral@gmail.com>
2025-12-07 02:31:32 +0800

    feat[Barral]: Updated the routing configuration in the api.php

--- server/routes/api.php
+++ server/routes/api.php
@@ -40,20 +40,39 @@ Route::prefix('/v1')->group(function () {
     Route::prefix('/landlord')->group(function () {
         
 
-        Route::get('/properties', [PropertyController::class, 'getOwnedProperties'])
-            ->middleware(['jwt.auth', 'jwt.role:landlord']);
+        // Route::get('/properties', [PropertyController::class, 'getOwnedProperties'])
+        //     ->middleware(['jwt.auth', 'jwt.role:landlord']);
 
-        Route::get('/properties/rooms', [RoomController::class , 'getAllRooms'])
-            ->middleware(['jwt.auth','jwt.role:landlord,admin']);
+        // Route::get('/properties/rooms', [RoomController::class , 'getAllRooms'])
+        //     ->middleware(['jwt.auth','jwt.role:landlord,admin']);
 
-        Route::get('/properties/{property_id}/rooms', [RoomController::class, 'getRoomByProperty'])
-            ->middleware(['jwt.auth', 'jwt.role:landlord,admin']);
+        // Route::get('/properties/{property_id}/rooms', [RoomController::class, 'getRoomByProperty'])
+        //     ->middleware(['jwt.auth', 'jwt.role:landlord,admin']);
 
-        Route::get('/properties/{property_id}/rooms/{room_id}', [RoomController::class, 'getRoomDetailsById'])
-            ->middleware(['jwt.auth', 'jwt.role:landlord,admin']);
+        // Route::get('/properties/{property_id}/rooms/{room_id}', [RoomController::class, 'getRoomDetailsById'])
+        //     ->middleware(['jwt.auth', 'jwt.role:landlord,admin']);
 
-        // Route::get('/property/{id}', [PropertyController::class, 'getProperties'])
-        //     ->middleware('jwt.role:landlord');
+
+        Route::middleware(['jwt.auth', 'jwt.role:landlord,admin'])
+            ->prefix('/properties')
+            ->group(function () {
+                // URL: /landlord/properties
+                Route::get('/', [PropertyController::class, 'getOwnedProperties']);
+
+                // URL: /landlord/properties/rooms
+                Route::get('/rooms', [RoomController::class, 'getAllRooms']);
+
+                // URL: /landlord/properties/{property_id}/rooms
+                Route::get('/{property_id}/rooms', [RoomController::class, 'getRoomByProperty']);
+
+                // URL: /landlord/properties/{property_id}/rooms/{room_id}
+                Route::get('/{property_id}/rooms/{room_id}', [RoomController::class, 'getRoomDetailsById']);
+
+                // URL: /landlord/properties (PUT)
+                // You had an unfinished PUT route in your snippet:
+                Route::post('/create', [PropertyController::class, 'createPropertyAndRoom']);
+            });
+
     });
 
 });
