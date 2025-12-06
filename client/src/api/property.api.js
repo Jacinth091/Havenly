fa279bfe2234d689da88ce6968ccbbc7fecae97c
Jacinth <jacinthcedricbarral@gmail.com>
2025-12-06 22:00:55 +0800

    feat[Barral]: Added a get room per property api function

--- client/src/api/property.api.js
+++ client/src/api/property.api.js
@@ -66,3 +66,74 @@ export const getProperties = async (queryParams = {}) => {
     };
   }
 };
+
+export const getRoomByProperty = async (property_id, queryParams = {}) => {
+  try {
+    const token =
+      sessionStorage.getItem("auth_token") ||
+      localStorage.getItem("auth_token");
+
+    if (!token) {
+      return {
+        success: false,
+        message: "Invalid api call, No token provided!",
+      };
+    }
+
+    const defaultParams = {
+      current_page: 1,
+      last_page: 1,
+      total_items: 0,
+      limit: 10,
+    };
+    const finalParams = { ...defaultParams, ...queryParams };
+
+    const response = await axios.get(
+      `${backendConnection()}/landlord/properties/${property_id}/rooms`,
+      {
+        params: finalParams,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (response.status === 200) {
+      console.log("Response: ", response);
+      const payload = response.data.data;
+      return {
+        success: true,
+        message: "Properties successfully fetched!",
+        property: payload.property,
+        summary: payload.summary,
+        rooms: payload.rooms.data,
+        pagination: {
+          current_page: payload.rooms.current_page,
+          last_page: payload.rooms.last_page,
+          total_items: payload.rooms.total,
+          limit: payload.rooms.per_page,
+        },
+      };
+    } else {
+      return {
+        success: false,
+        message: "Rooms not fetched successfully!",
+        // Return empty structures on failure to prevent UI crashes
+        property: null,
+        summary: null,
+        rooms: [],
+        pagination: defaultParams,
+      };
+    }
+  } catch (error) {
+    console.error("API Error:", error);
+    return {
+      success: false,
+      message: error.response?.data?.message || "Failed to fetch rooms",
+      error: error,
+    };
+  }
+};
+
+
