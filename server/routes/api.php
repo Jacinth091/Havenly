0e66f90ab033cf23ce1a8204f05a75a1902e1aea
Jacinth <jacinthcedricbarral@gmail.com>
2025-12-09 14:16:54 +0800

    feat[Barral]: Added more routes, POST for createing property and room, POST for creating Rooms, a /tenants route that has GET for fetching tenants data, and /lease route that has POST route that creates lease and assign tenant to a property room

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\LeaseController;
 use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\RoomController;
+use App\Http\Controllers\TenantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
@@ -42,23 +44,41 @@ Route::prefix('/v1')->group(function () {
             ->prefix('/properties')
             ->group(function () {
                 // URL: /landlord/properties
-                Route::get('/', [PropertyController::class, 'getOwnedProperties']);
+            Route::get('/', [PropertyController::class, 'getOwnedProperties']);
 
-                // URL: /landlord/properties/rooms
-                Route::get('/rooms', [RoomController::class, 'getAllRooms']);
+            // URL: /landlord/properties/rooms
+            Route::get('/rooms', [RoomController::class, 'getAllRooms']);
 
-                // URL: /landlord/properties/{property_id}/rooms
-                Route::get('/{property_id}/rooms', [RoomController::class, 'getRoomByProperty']);
+            // URL: /landlord/properties/{property_id}/rooms
+            Route::get('/{property_id}/rooms', [RoomController::class, 'getRoomByProperty']);
 
-                // URL: /landlord/properties/{property_id}/rooms/{room_id}
-                Route::get('/{property_id}/rooms/{room_id}', [RoomController::class, 'getRoomDetailsById']);
+            // URL: /landlord/properties/{property_id}/rooms/{room_id}
+            Route::get('/{property_id}/rooms/{room_id}', [RoomController::class, 'getRoomDetailsById']);
 
-                // URL: /landlord/properties/create (POST)
-                Route::post('/create', [PropertyController::class, 'createPropertyAndRoom']);
-                
-                // URL: /landlord/properties/{propertyId}/rooms/create
-                Route::post('/{propertyId}/rooms/create', [RoomController::class, 'createRooms']);
-            });
+            // URL: /landlord/properties/create (POST)
+            Route::post('/create', [PropertyController::class, 'createPropertyAndRoom']);
+            
+            // URL: /landlord/properties/{propertyId}/rooms/create
+            Route::post('/{propertyId}/rooms/create', [RoomController::class, 'createRooms']);
+
+
+
+        });
+        Route::middleware(['jwt.auth', 'jwt.role:landlord,admin'])
+            ->prefix('/tenants')
+            ->group(function () {
+
+            Route::get('/', [TenantController::class, 'getTenantsWithLease']);
+            Route::get('/available', [TenantController::class, 'getAvailableTenants']);
+            Route::get('{propertyId}', [TenantController::class, 'getTenantsInProperty']);
+        });
+
+        Route::middleware(['jwt.auth', 'jwt.role:landlord,admin'])
+            ->prefix('/lease')
+            ->group(function () {
+
+            Route::post('/create', [LeaseController::class, 'createLeaseAndAssignTenantToProperty']);
+        });
 
     });
 
